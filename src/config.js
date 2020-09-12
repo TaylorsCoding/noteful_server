4d81b514539fac0b929de4ca6b063ce8bd9356d5
codingbeginner19 <60525942+codingbeginner19@users.noreply.github.com>
2020-09-12 18:27:18 -0400

    change db url

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgresql://codingbeginner19n@localhost/notefuldb",
+    "postgres://xtirzohequzmwj:2d1838e55cfb7d572755d8c509458690ce8976293260b226979fd44f099016aa@ec2-54-236-146-234.compute-1.amazonaws.com:5432/dn41bj94458bc",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://codingbeginner19n@localhost/notefuldb",
